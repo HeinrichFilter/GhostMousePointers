6f07702fa34add0be95f0669ac54e8d32bd3e01f
Heinrich Filter <dev@filter.co.za>
2010-08-11 16:30:25 +0200

    Clean up and added pointer image that now works.
    Also added code that deletes a GhostMousePointer when the window is closed.
    Deleted unneeded comments and files.

--- src/main/scala/com/heinrichfilter/commucontext/comet/Mouse.scala
+++ src/main/scala/com/heinrichfilter/commucontext/comet/Mouse.scala
@@ -1,23 +1,18 @@
 package com.heinrichfilter.commucontext.comet
 
-import java.util.Date
 import net.liftweb.actor.LiftActor
-import net.liftweb.common.Box
-import net.liftweb.common.Full
+import net.liftweb.common._
 import net.liftweb.http._
-import net.liftweb.http.js.JE.Call
+import net.liftweb.http.js.JE._
 import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.jquery._
+import net.liftweb.http.js.{JE,JsCmd,JsCmds}
 import net.liftweb.util.Helpers._
 import net.liftweb.util._
-import net.liftweb.http.js.jquery._
-import net.liftweb.http.js.{JE,JsCmd,JsCmds, Jx}
 import JsCmds._ // For implicits
 import JqJE._
-import net.liftweb.http.js.JE._
-import JE.{JsRaw,Str}
-import scala.xml.NodeSeq
+import JE._
 import scala.xml.Text
-import scala.xml.Unparsed
 
 object MouseMaster extends LiftActor {
   private var mouseList : List[Mouse] = Nil
@@ -25,62 +20,47 @@ object MouseMaster extends LiftActor {
   protected def messageHandler = {
     case SubscribeMouse(mouse) => {
         mouseList ::= mouse
-        println("------SubscribeMouse. Mousecount:" + mouseList.length)
       }
     case UnsubscribeMouse(mouse) => {
         mouseList -= mouse
-        println("------UnsubscribeMouse. Mousecount:" + mouseList.length)
+        this ! DeleteGhostMouseInAllViews(mouse.uniqueId)
       }
-    case UpdateAllGhostMouseCoords(uniqueId, mouseCoords) => {
+    case UpdateGhostMouseInAllViews(uniqueId, mouseCoords) => {
         mouseList.foreach(_ ! UpdateMouseCoords(uniqueId, mouseCoords))
       }
+    case DeleteGhostMouseInAllViews(uniqueId) => {
+        mouseList.foreach(_ ! DeleteGhostMouse(uniqueId))
+    }
   }
 }
 
 class Mouse extends CometActor {
   var mouseCoords = new MouseCoords(0,0)
   override def defaultPrefix = Full("mouse")
-  //def render = bind("sendCoordsToServer" -> Text(SHtml.ajaxCall(Str("Button-2"), onMouseMoveServer _)._2 +""),
   def render = bind("sendCoordsToServer" ->      Text(""+SHtml.ajaxCall(JsRaw("e.pageX + ',' + e.pageY"), onMouseMoveServer _)._2),
-                    "sendUnbindEventToServer" -> Text(""+SHtml.ajaxCall(JsRaw("'noop'"), onUnbindServer _)._2),
-                    "otherPointers" -> <span id="text">otherPointers</span>
+                    "sendUnbindEventToServer" -> Text(""+SHtml.ajaxCall(JsRaw("'noop'"), onUnbindServer _)._2)
   )
 
   override def lowPriority : PartialFunction[Any, Unit] = {
-    case Tick => {
-        println("Got tick " + new Date());
-        partialUpdate(SetHtml("text", Text(timeNow.toString)))
+    case UpdateMouseCoords(id, mouseCoords) => {
+        partialUpdate(Call("updateOrCreateMouseDiv", id, mouseCoords.x, mouseCoords.y))
       }
-    case UpdateMouseCoords(uniqueId, mouseCoords) => {
-        println("##################################### UpdateMouseCoords Start " + uniqueId)
-        partialUpdate(Call("updateOrCreateMouseDiv", uniqueId, mouseCoords.x + "," + mouseCoords.y))
-        println("##################################### UpdateMouseCoords End " + uniqueId)
-    }
-    case CreateDivForMouse(uniqueId) => {
-        println("##################################### CreateDivForMouse Start")
-        //partialUpdate(Jq("body") ~> JqAppend(<div id = "{uniqueId}">CreateDivForMouse{uniqueId}</div>))
-        //partialUpdate(JsRaw("$('body').append('<div id=" + uniqueId + ">Test</p>');Alert('test');"))
-        partialUpdate(Call("updateOrCreateMouseDiv", uniqueId, "test"))
-        println("##################################### CreateDivForMouse End")
+    case DeleteGhostMouse(id) => {
+        partialUpdate(JqId("mouse_" + id) ~> JqRemove())
     }
   }
   
   def onMouseMoveServer (str: String) : JsCmd = {
     mouseCoords.setCoords(str)
-    MouseMaster ! UpdateAllGhostMouseCoords(this.uniqueId, this.mouseCoords)
+    MouseMaster ! UpdateGhostMouseInAllViews(uniqueId, mouseCoords)
     println("Received " + str)
-    //partialUpdate(SetHtml("text", Text(str + " - " +timeNow.toString)))
     Noop
-    //JsRaw("alert('Button2 clicked')")
   }
 
   def onUnbindServer (str: String) : JsCmd = {
-    this.localShutdown
-    //unWatch ???
-    println("-------onUnbindServer")
+    MouseMaster ! DeleteGhostMouseInAllViews(this.uniqueId)
     Noop
   }
-
 
   override def localSetup {
     MouseMaster ! SubscribeMouse(this)
@@ -88,11 +68,9 @@ class Mouse extends CometActor {
   }
 
   override def localShutdown {
-    println("-------localShutdown")
     MouseMaster ! UnsubscribeMouse(this)
     super.localShutdown()
   }
-
 }
 
 class MouseCoords(var x : Int, var y : Int) {
@@ -103,8 +81,9 @@ class MouseCoords(var x : Int, var y : Int) {
   }
 }
 
-case class CreateDivForMouse(uniqueId : String)
 case class SubscribeMouse(mouse : Mouse)
 case class UnsubscribeMouse(mouse : Mouse)
-case class UpdateMouseCoords(uniqueId:String, newMouseCoords:MouseCoords)
-case class UpdateAllGhostMouseCoords(uniqueId:String, newMouseCoords:MouseCoords)
+case class UpdateMouseCoords(ghostMouseId:String, newMouseCoords:MouseCoords)
+case class DeleteGhostMouse(ghostMouseId:String)
+case class UpdateGhostMouseInAllViews(ghostMouseId:String, newMouseCoords:MouseCoords)
+case class DeleteGhostMouseInAllViews(ghostMouseId:String)
